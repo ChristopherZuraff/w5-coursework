ec892fa225fd5d3f8d4e3d18dc36c4148d0d287d
ChristopherZuraff <36679712+ChristopherZuraff@users.noreply.github.com>
2018-06-21 10:31:48 -0700

    Add files via upload

---
+++ exeterW5L1/src/exeterW5L1/W5L1.java
@@ -0,0 +1,62 @@
+package exeterW5L1;
+
+public class W5L1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//step1
+//		boolean isSunny = true;
+//
+//		 if (isSunny) {
+//		     System.out.println("Wear sunglasses!");
+//		 } else {
+//		     System.out.println("Don't wear sunglasses."); 
+//	}
+		 //step1 end
+		 
+		//step2
+//		 boolean isSunny = false;
+//		 boolean atBeach = false;
+//
+//		 // if it's sunny
+//		     // print "Wear sunglasses!"
+//		     // if at the beach
+//		         // print "Wear sunblock!"
+//		 
+//		 if (isSunny && atBeach) {
+//		     System.out.println("Wear sunglasses & wear sunblock!");
+//		 } else if (isSunny) {
+//		     System.out.println(" Where sunblock! Don't wear sunglasses.");
+//		    
+//		 } else if (atBeach) {System.out.println("Wear sunblock! Don't need sunglasses.");
+//		 }else {System.out.println("Don't need to wear sunglasses");
+//	}
+
+//		 step2 end    
+		 // else
+		         // print "Don't need sunblock."
+
+		 // else
+		     // print "Don't need to wear sunglasses."
+//step 3
+		 boolean isSunny = false;
+		 boolean atBeach = false;
+
+		 // if it's sunny
+		     // print "Wear sunglasses!"
+		     // if at the beach
+		         // print "Wear sunblock!"
+		 
+		 if (isSunny && atBeach) {
+		     System.out.println("Wear sunglasses & wear sunblock!");
+		 } else if (isSunny) {
+		     System.out.println(" Don't wear sunglasses & come back tomorrow!");
+		    
+		 } else if (atBeach) {System.out.println("Wear sunblock! Don't need sunglasses.");
+		 }else {System.out.println("Don't need to wear sunglasses & don't go to the beach!");
+	}
+}
+	//step3 end
+	}
+
